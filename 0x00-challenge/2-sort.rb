748d0cf8ff046c39d632dc0dd099793c0a00816c
himza25 <himza.of.theking95@gmail.com>
2023-11-26 17:03:16 -0800

    task 2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -4,27 +4,13 @@
 #
 ###
 
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-
-    # convert to integer
-    i_arg = arg.to_i
-
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] >= i_arg
-            result.insert(i, i_arg)
-            is_inserted = true
-        else
-            i += 1
-        end
-    end
-    result << i_arg if !is_inserted
+result = ARGV.filter_map do |arg|
+  # Convert arg to integer if it is a valid integer string, nil otherwise
+  arg.match?(/^[-+]?\d+$/) ? arg.to_i : nil
 end
 
+# Sort the array of integers
+result.sort!
+
+# Output sorted integers
 puts result.join(' ')
