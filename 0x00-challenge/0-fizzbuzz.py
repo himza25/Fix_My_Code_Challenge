db9e3bd8e7b8d6a1b2fb87a02ec8242d05adad81
himza25 <himza.of.theking95@gmail.com>
2023-11-26 16:35:17 -0800

    task 0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -3,11 +3,9 @@
 """
 import sys
 
-
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -26,7 +24,6 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
-
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
